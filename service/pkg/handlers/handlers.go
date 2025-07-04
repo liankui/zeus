67d1c7016c52c8ebd3207cef2c03186cfac5c503
Eric Yue <yuekewin@gmail.com>
2025-07-04 17:41:50 +0800

    feat: impl handler

--- service/pkg/handlers/handlers.go
+++ service/pkg/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"os/exec"
 
 	"github.com/chaos-io/chaos/logs"
 
@@ -22,9 +23,30 @@ func NewService() pb.ZeusServer {
 func (s zeusServer) ExecSoar(ctx context.Context, in *pb.ExecSoarRequest) (*pb.ExecSoarResponse, error) {
 	logs.Infow("ExecSoar", "request", in)
 
+	soar := in.GetSoar()
+	if soar == nil {
+		return &pb.ExecSoarResponse{}, logs.NewErrorf("soar NOT be nil")
+	}
+
+	cmd := exec.Command("soar")
+	if len(soar.Query) > 0 {
+		cmd.Args = append(cmd.Args, "-query", soar.Query)
+	}
+
+	if len(soar.ReportType) > 0 {
+		cmd.Args = append(cmd.Args, "-report", soar.ReportType)
+	}
+
+	logs.Infow("exec soar", "cmd", cmd.String())
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		logs.Errorw("failed to exec soar", "cmd", cmd.String(), "error", err)
+	}
+
 	resp := &pb.ExecSoarResponse{
 		// Level:
-		// Data:
+		Data: string(output),
 	}
 	return resp, nil
 }
